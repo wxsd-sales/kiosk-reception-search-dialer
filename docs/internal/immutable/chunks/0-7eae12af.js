906e87d229a3dc1e3124832c9a538012824a59aa
wazquez <victorvazquezv@gmail.com>
2025-06-20 10:53:09 +0200

    one more build no in main

---
+++ docs/internal/immutable/chunks/0-7eae12af.js
@@ -0,0 +1,1 @@
+import{default as r}from"../components/pages/_layout.svelte-7f8b77e8.js";import"./singletons-4e09a5de.js";import"./stores-08263bda.js";export{r as component};
--- docs/internal/immutable/chunks/1-95941554.js
+++
@@ -1,1 +0,0 @@
-import{default as r}from"../components/error.svelte-37517ae8.js";import"./singletons-1909b05a.js";import"./stores-f6e2c3dc.js";export{r as component};
