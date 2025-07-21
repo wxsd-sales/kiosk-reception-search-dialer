749faba664861d99d123163e2d52e3db182549d8
wazquez <victorvazquezv@gmail.com>
2025-07-21 14:55:51 +0200

    no changes

---
+++ docs/internal/immutable/chunks/1-63146189.js
@@ -0,0 +1,1 @@
+import{default as r}from"../components/error.svelte-87c3c1c5.js";import"./singletons-65d6426f.js";import"./stores-b02ab38b.js";export{r as component};
--- docs/internal/immutable/chunks/2-bf5629ea.js
+++
@@ -1,1 +0,0 @@
-import{default as r}from"../components/pages/_page.svelte-91745357.js";import"./singletons-13032f40.js";import"./stores-13cf2b0d.js";export{r as component};
